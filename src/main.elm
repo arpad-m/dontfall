fe7a3896cd4f0e205f035435294f2517d735b321
Arpad Müller <arpad-m@users.noreply.github.com>
2016-06-28 08:25:18 +0200

    Use Html.App architecture

--- src/main.elm
+++ src/main.elm
@@ -2,10 +2,23 @@ import Collage exposing (..)
 import Element exposing (..)
 import Text exposing (..)
 import Html exposing (..)
+import Html.App
 import Color
 
 b : Color.Color
 b = Color.rgb 67 76 67
 
-main : Html msg
-main = toHtml (collage 300 600 [filled b (rect 40 50)])
+type GameData = A | B | C
+
+renderScene : GameData -> Html msg
+renderScene d = toHtml (collage 300 600 [filled b (rect 40 50)])
+
+updateScene : msg -> GameData -> GameData
+updateScene m d = A
+
+main : Program Never
+main = Html.App.beginnerProgram
+ { model = B
+ , view = renderScene
+ , update = updateScene
+ }
