cf2cfef6179f9b328831c8fd82df4880b4a41152
Arpad Müller <arpad-m@users.noreply.github.com>
2016-06-28 08:25:32 +0200

    Add Platform drawing

--- src/Render.elm
+++ src/Render.elm
@@ -6,11 +6,20 @@ import Color
 
 import BaseTypes exposing (..)
 
-b : Color.Color
-b = Color.rgb 67 76 67
+platformColor : Color.Color
+platformColor = Color.rgb 30 19 67
+
+plFill : Shape -> Form
+plFill = (filled platformColor)
+
+platformModel : Form
+platformModel = group [plFill (rect 60 16), move (-30, 0) (plFill (circle 8)), move (30, 0) (plFill (circle 8))]
+
+playerColor : Color.Color
+playerColor = Color.rgb 0 128 0
 
 playerModel : Form
-playerModel = filled b (rect 40 50)
+playerModel = filled playerColor (rect 40 50)
 
 renderScene : Int -> Int -> GameData -> Element
 renderScene width height d = collage width height [move (d.characterPosX - (toFloat width / 2), 0) playerModel]
