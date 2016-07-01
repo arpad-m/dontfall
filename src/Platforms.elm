7990b7c13d2275c82e05f2fa853151e91a9613ac
Arpad Müller <arpad-m@users.noreply.github.com>
2016-07-01 07:36:25 +0200

    Move Platform generation to own file

---
+++ src/Platforms.elm
@@ -0,0 +1,38 @@
+module Platforms exposing (..)
+
+import Random exposing (..)
+import Murmur3
+
+oneIn : Int -> Generator Bool
+oneIn i = map ((==) 0) (int 0 i)
+
+maybeRandom : Generator Bool -> Generator a -> Generator (Maybe a)
+maybeRandom bgen gen =
+    map2 (\b -> \c -> if b then Just c else Nothing) bgen gen
+
+seedAtPoint : Int -> Int -> Seed
+seedAtPoint i j = initialSeed <| Murmur3.hashString i (toString j)
+
+worldRandomizeAtPoint : Int -> Int -> Generator a -> a
+worldRandomizeAtPoint worldSeed p gen = let (s, _) = step gen (seedAtPoint worldSeed p) in s
+
+rangeTo : Int -> List Int
+rangeTo a = List.indexedMap (\i -> \v -> i) (List.repeat a ())
+
+platformDistance : Float
+platformDistance = 30
+
+pldf : Float -> Int
+pldf f = floor (f / platformDistance)
+
+ipldf : Int -> Float
+ipldf f = (toFloat f) * platformDistance
+
+getWorldPlatforms { worldSeed, flWidth } yOffs pixeldiff =
+    let (oldYInt, newYInt) = (pldf yOffs, pldf (yOffs + pixeldiff)) in
+        List.filterMap
+            (\n -> worldRandomizeAtPoint worldSeed (n + oldYInt)
+                (maybeRandom (oneIn 2) (Random.map (\x -> (x, ipldf (n + oldYInt))) (float 0 flWidth)))
+            )
+            (rangeTo (newYInt - oldYInt))
+
